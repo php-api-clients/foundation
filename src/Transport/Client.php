925acd39c46baeac1cdba79c950ba648f5e69c85
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-05-17 21:31:24 +0200

    Client

---
+++ src/Transport/Client.php
@@ -0,0 +1,128 @@
+<?php
+declare(strict_types=1);
+
+namespace WyriHaximus\ApiClient\Transport;
+
+use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Psr7\Request;
+use Psr\Http\Message\ResponseInterface;
+use React\Cache\CacheInterface;
+use React\EventLoop\LoopInterface;
+use React\Promise\Deferred;
+use React\Promise\FulfilledPromise;
+use React\Promise\PromiseInterface;
+use function React\Promise\reject;
+use function React\Promise\resolve;
+use function WyriHaximus\React\futureFunctionPromise;
+
+class Client
+{
+    const DEFAULT_OPTIONS = [
+        'schema' => 'https',
+        'path' => '/',
+        'user_agent' => 'WyriHaximus/php-api-client',
+        'headers' => [],
+    ];
+
+    /**
+     * @var GuzzleClient
+     */
+    protected $handler;
+
+    /**
+     * @var LoopInterface
+     */
+    protected $loop;
+
+    /**
+     * @var array
+     */
+    protected $options = [];
+
+    /**
+     * @var Hydrator
+     */
+    protected $hydrator;
+
+    /**
+     * @var CacheInterface
+     */
+    protected $cache;
+
+    public function __construct(LoopInterface $loop, GuzzleClient $handler, $options = [])
+    {
+        $this->loop = $loop;
+        $this->handler = $handler;
+        $this->options = self::DEFAULT_OPTIONS + $options;
+        if (isset($this->options['cache']) && $this->options['cache'] instanceof CacheInterface) {
+            $this->cache = $this->options['cache'];
+        }
+        $this->hydrator = new Hydrator($this, $options);
+    }
+
+    public function request(string $path, bool $refresh = false): PromiseInterface
+    {
+        if ($refresh) {
+            return $this->sendRequest($path);
+        }
+
+        return $this->checkCache($path)->otherwise(function () use ($path) {
+            return $this->sendRequest($path);
+        });
+    }
+
+    protected function checkCache(string $path)
+    {
+        if ($this->cache instanceof CacheInterface) {
+            return $this->cache->get($path)->then(function ($json) use ($path) {
+                return $this->jsonDecode($json);
+            });
+        }
+
+        return reject();
+    }
+
+    protected function sendRequest(string $path, string $method = 'GET'): PromiseInterface
+    {
+        $deferred = new Deferred();
+
+        $this->handler->sendAsync($this->createRequest($method, $path))->then(function (ResponseInterface $response) use ($deferred) {
+            $contents = $response->getBody()->getContents();
+            $deferred->resolve($contents);
+        }, function ($error) use ($deferred) {
+            $deferred->reject($error);
+        });
+
+        return $deferred->promise()->then(function ($json) use ($path) {
+            $this->cache->set($path, $json);
+            return $this->jsonDecode($json);
+        });
+    }
+
+    protected function createRequest(string $method, string $path)
+    {
+        $url = $this->options['schema'] . '://' . $this->options['host'] . $this->options['path'] . $path;
+        $headers = [
+            'User-Agent' => $this->options['user_agent'],
+        ];
+        $headers += $this->options['headers'];
+        return new Request($method, $url, $headers);
+    }
+
+    protected function jsonDecode($json): PromiseInterface
+    {
+        return futureFunctionPromise($this->loop, $json, function ($json) {
+            return json_decode($json, true);
+        });
+    }
+
+    public function getHydrator(): Hydrator
+    {
+        return $this->hydrator;
+    }
+
+    public function getLoop(): LoopInterface
+    {
+        return $this->loop;
+    }
+}
---
+++ tests/Transport/ClientTest.php
@@ -0,0 +1,139 @@
+<?php
+declare(strict_types=1);
+
+namespace WyriHaximus\Tests\AppVeyor\Transport;
+
+use GuzzleHttp\Promise\FulfilledPromise;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
+use Phake;
+use GuzzleHttp\Client as GuzzleClient;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamInterface;
+use React\Cache\CacheInterface;
+use React\EventLoop\Factory;
+use React\EventLoop\LoopInterface;
+use React\Promise\RejectedPromise;
+use WyriHaximus\ApiClient\Transport\Client;
+use WyriHaximus\ApiClient\Transport\Hydrator;
+use function Clue\React\Block\await;
+
+class ClientTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetLoop()
+    {
+        $client = new Client(
+            Factory::create(),
+            Phake::mock(GuzzleClient::class)
+        );
+        $this->assertInstanceOf(LoopInterface::class, $client->getLoop());
+    }
+
+    public function testGetHydrator()
+    {
+        $client = new Client(
+            Phake::mock(LoopInterface::class),
+            Phake::mock(GuzzleClient::class)
+        );
+        $this->assertInstanceOf(Hydrator::class, $client->getHydrator());
+    }
+
+    public function testRequest()
+    {
+        $loop = Factory::create();
+
+        $stream = Phake::mock(StreamInterface::class);
+        Phake::when($stream)->getContents()->thenReturn(json_encode([
+            'foo' => 'bar',
+        ]));
+
+        $response = Phake::mock(ResponseInterface::class);
+        Phake::when($response)->getBody()->thenReturn($stream);
+
+        $request = false;
+        $handler = Phake::mock(GuzzleClient::class);
+        Phake::when($handler)->sendAsync($this->isInstanceOf(Request::class))->thenReturnCallback(function (RequestInterface $guzzleRequest) use ($response, &$request) {
+            $request = $guzzleRequest;
+            return new FulfilledPromise($response);
+        });
+
+        $client = new Client(
+            $loop,
+            $handler,
+            [
+                'host' => 'api.example.com',
+            ]
+        );
+
+        $client->request('status', true);
+
+        $this->assertSame('GET', $request->getMethod());
+        $this->assertSame('https://api.example.com/status', (string) $request->getUri());
+        $this->assertSame([
+            'Host' => ['api.example.com'],
+            'User-Agent' => ['WyriHaximus/php-api-client'],
+        ], $request->getHeaders());
+    }
+
+    public function testRequestRefreshHitAPI()
+    {
+        $loop = Factory::create();
+
+        $cache = Phake::mock(CacheInterface::class);
+
+        $stream = Phake::mock(StreamInterface::class);
+        Phake::when($stream)->getContents()->thenReturn(json_encode([
+            'foo' => 'bar',
+        ]));
+
+        $response = Phake::mock(Response::class);
+        Phake::when($response)->getBody()->thenReturn($stream);
+
+        $handler = Phake::mock(GuzzleClient::class);
+        Phake::when($handler)->sendAsync($this->isInstanceOf(Request::class))->thenReturn(new FulfilledPromise($response));
+
+        $client = new Client(
+            $loop,
+            $handler,
+            [
+                'host' => 'api.example.com',
+            ]
+        );
+
+        $client->request('status', true);
+        $loop->run();
+
+        Phake::verify($handler)->sendAsync($this->isInstanceOf(Request::class));
+        Phake::verify($cache, Phake::never())->get('status');
+    }
+
+    public function testRequestNoCacheHitAPI()
+    {
+        $loop = Factory::create();
+
+        $stream = Phake::mock(StreamInterface::class);
+        Phake::when($stream)->getContents()->thenReturn(json_encode([
+            'foo' => 'bar',
+        ]));
+
+        $response = Phake::mock(Response::class);
+        Phake::when($response)->getBody()->thenReturn($stream);
+
+        $handler = Phake::mock(GuzzleClient::class);
+        Phake::when($handler)->sendAsync($this->isInstanceOf(Request::class))->thenReturn(new FulfilledPromise($response));
+
+        $client = new Client(
+            $loop,
+            $handler,
+            [
+                'host' => 'api.example.com',
+            ]
+        );
+
+        $client->request('status', true);
+        $loop->run();
+
+        Phake::verify($handler)->sendAsync($this->isInstanceOf(Request::class));
+    }
+}
