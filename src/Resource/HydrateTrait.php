0064b7d4b49cc122e6351071d03a3c276deab77f
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-05-21 22:52:50 +0200

    Missing return type

--- src/Resource/HydrateTrait.php
+++ src/Resource/HydrateTrait.php
@@ -10,7 +10,7 @@ trait HydrateTrait
 {
     abstract protected function getTransport(): Client;
 
-    protected function hydrate(string $class, array $json)
+    protected function hydrate(string $class, array $json): ResourceInterface
     {
         return $this->getTransport()->getHydrator()->hydrate($class, $json);
     }
