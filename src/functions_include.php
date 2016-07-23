ba2ee6a81b2822935be42a8f3fc3690baa75252a
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-07-23 19:33:21 +0200

    functions_include to make sure functions is only included once

---
+++ src/functions_include.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace WyriHaximus\ApiClient;
+
+// @codeCoverageIgnoreStart
+if (!function_exists('WyriHaximus\ApiClient\debug_print')) {
+    require __DIR__ . '/functions.php';
+}
+// @codeCoverageIgnoreEnd
