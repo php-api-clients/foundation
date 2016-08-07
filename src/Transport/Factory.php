8f64b91a1a21249f34886aebf138767c003dc62a
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-08-07 22:10:23 +0200

    Added createFromReactHttpClient to the Factory so shared HTTP Clients can be added

--- src/Transport/Factory.php
+++ src/Transport/Factory.php
@@ -5,8 +5,12 @@ namespace ApiClients\Foundation\Transport;
 
 use GuzzleHttp\Client as GuzzleClient;
 use GuzzleHttp\HandlerStack;
+use React\Dns\Resolver\Resolver;
 use React\EventLoop\Factory as LoopFactory;
 use React\EventLoop\LoopInterface;
+use React\HttpClient\Client as HttpClient;
+use React\HttpClient\Factory as HttpClientFactory;
+use React\Dns\Resolver\Factory as ResolverFactory;
 use WyriHaximus\React\GuzzlePsr7\HttpClientAdapter;
 
 class Factory
@@ -22,12 +26,44 @@ class Factory
             $loop = LoopFactory::create();
         }
 
+        if (!isset($options['dns'])) {
+            $options['dns'] = '8.8.8.8';
+        }
+
+        $resolver = (new ResolverFactory())->createCached($options['dns'], $loop);
+        $httpClient = (new HttpClientFactory())->create($loop, $resolver);
+
+        return self::createFromReactHttpClient(
+            $httpClient,
+            $resolver,
+            $loop,
+            $options
+        );
+    }
+
+    /**
+     * @param HttpClient $httpClient
+     * @param Resolver $resolver
+     * @param LoopInterface|null $loop
+     * @param array $options
+     * @return Client
+     */
+    public static function createFromReactHttpClient(
+        HttpClient $httpClient,
+        Resolver $resolver,
+        LoopInterface $loop = null,
+        array $options = []
+    ): Client {
         return new Client(
             $loop,
             new GuzzleClient(
                 [
                     'handler' => HandlerStack::create(
-                        new HttpClientAdapter($loop)
+                        new HttpClientAdapter(
+                            $loop,
+                            $httpClient,
+                            $resolver
+                        )
                     ),
                 ]
             ),
