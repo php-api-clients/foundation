f9a93a37284847c4b90c952e2d1cfbef17016f42
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-08-11 07:56:44 +0200

    Indented options

--- src/Transport/Options.php
+++ src/Transport/Options.php
@@ -4,14 +4,14 @@ namespace ApiClients\Foundation\Transport;
 
 class Options
 {
-    const DNS = 'dns';
-    const CACHE = 'cache';
-    const HYDRATOR = 'hydrator';
+    const DNS              = 'dns';
+    const CACHE            = 'cache';
+    const HYDRATOR         = 'hydrator';
     const HYDRATOR_OPTIONS = 'hydrator_options';
-    const SCHEMA = 'schema';
-    const HOST = 'host';
-    const PORT = 'port';
-    const PATH = 'path';
-    const USER_AGENT = 'user_agent';
-    const HEADERS = 'headers';
+    const SCHEMA           = 'schema';
+    const HOST             = 'host';
+    const PORT             = 'port';
+    const PATH             = 'path';
+    const USER_AGENT       = 'user_agent';
+    const HEADERS          = 'headers';
 }
