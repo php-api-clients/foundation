1bf7027cfc5e4a1f513241eb3a849a6a74674c55
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-07-09 11:11:02 +0200

    Added extract and hydrate to client interface

--- src/ClientInterface.php
+++ src/ClientInterface.php
@@ -2,6 +2,7 @@
 
 namespace ApiClients\Foundation;
 
+use ApiClients\Foundation\Resource\ResourceInterface;
 use Psr\Container\ContainerInterface;
 use React\Promise\CancellablePromiseInterface;
 
@@ -22,4 +23,16 @@ interface ClientInterface
      * @return CancellablePromiseInterface
      */
     public function handle($command): CancellablePromiseInterface;
+
+    /**
+     * @param string $resource
+     * @return CancellablePromiseInterface
+     */
+    public function hydrate(string $resource): CancellablePromiseInterface;
+
+    /**
+     * @param ResourceInterface $resource
+     * @return CancellablePromiseInterface
+     */
+    public function extract(ResourceInterface $resource): CancellablePromiseInterface;
 }
